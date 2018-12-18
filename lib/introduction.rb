8065bef9abde85bb4f47ed3d0e3a0ab052063a8c
roy renno <royrenno@yahoo.com>
2018-12-18 07:03:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,3 @@
-def introduction_with_language("Dan")
-  puts "Hi, my name is #{Bob} and I am learning to program in #{Ember.js}."
-end
+def introduction_with_language(Dan)
+  puts "Hi, my name is Dan"
 end
